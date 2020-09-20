4335b6721bab194d387be3e6afad93dcde8a1873
Lukman Sanni <lukmansanni60@gmail.com>
2020-09-20 11:07:57 +0100

    add form-unresponsive

--- src/js/models/model.js
+++ src/js/models/model.js
@@ -1,3 +1,7 @@
 export const model =()=>{
     console.log('Hello, This is coming from model.js')
 }
+
+
+//// USE local storage to check if the question hasn't been 
+///// received by a perticular client before.
